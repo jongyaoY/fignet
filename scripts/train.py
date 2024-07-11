ce00c4633ea44f278f9059ca3216f1c268f21537
zyi <zoyi01@dfki.de>
2024-07-11 17:25:10 +0200

    add license

--- scripts/train.py
+++ scripts/train.py
@@ -1,12 +1,33 @@
-import torch
+# MIT License
+#
+# Copyright (c) [2024] [Zongyao Yi]
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
+import torch
 from robosuite.utils.binding_utils import MjRenderContextOffscreen, MjSim
 
 from fignet.logger import Logger
 from fignet.simulator import LearnedSimulator
 from fignet.trainer import Trainer
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     mujoco_model_path = "test_models/mujoco_scene.xml"
 
@@ -33,11 +54,11 @@ if __name__ == '__main__':
         "save_video": True,
     }
     if torch.cuda.is_available():
-        device = torch.device('cuda')
-        print('Using cuda')
+        device = torch.device("cuda")
+        print("Using cuda")
     else:
-        device = torch.device('cpu')
-        print('using cpu')
+        device = torch.device("cpu")
+        print("using cpu")
     sim = LearnedSimulator(
         mesh_dimensions=3,
         latent_dim=latent_dim,
@@ -50,4 +71,3 @@ if __name__ == '__main__':
     logger = Logger(config)
     trainer = Trainer(sim=sim, ref_sim=ref_sim, logger=logger, config=config)
     trainer.train()
-    
