718440ad92a857c0676bf35f04c9cc602fe04396
zyi <zoyi01@dfki.de>
2024-07-11 16:50:42 +0200

    update license

--- fignet/logger.py
+++ fignet/logger.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) [2024] [Zongyao Yi]
+# Copyright (c) 2021 Stanford Interactive Perception and Robot Learning Lab
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
