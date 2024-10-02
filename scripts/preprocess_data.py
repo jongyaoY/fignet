40fe41ca2b41de7f6b9d5ce95e5a05e7283b75aa
zyi <zongyao.yi@dfki.de>
2024-10-02 20:54:29 +0200

    use multithreading to preprocess data

--- scripts/preprocess_data.py
+++ scripts/preprocess_data.py
@@ -21,16 +21,22 @@
 # SOFTWARE.
 
 import argparse
+import concurrent.futures
+import logging
 import os
 import pickle
 import sys
+from multiprocessing import Manager
 from pathlib import Path
 
+import numpy as np
 import torch
 import tqdm
 import yaml
 
-from fignet.data_loader import MujocoDataset
+from fignet.mujoco_extensions.mj_classes import MjSimLearned
+from fignet.mujoco_extensions.mj_scene import get_scene_info
+from fignet.mujoco_extensions.physics_state_tracker import PhysicsStateTracker
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config_file", type=str, required=True)
@@ -41,10 +47,13 @@ args = parser.parse_args()
 
 data_path = args.data_path
 config_file = args.config_file
-num_workers = max(args.num_workers, 1)
-batch_size = min(2 * num_workers, 64)
+num_workers = min(args.num_workers, os.cpu_count())
+# batch_size = min(2 * num_workers, 64)
 output_path = os.path.join(Path(data_path).parent, Path(data_path).stem)
 device = torch.device("cpu")
+
+logger = logging.getLogger(__name__)
+# lock = Lock()
 
 
 def collate_fn(batch):
@@ -69,6 +78,55 @@ def save_graph(graph, graph_i, save_path):
             pickle.dump(graph_dict, f)
 
 
+def process_episode(traj, sim_cfg, ep_i, lock, counter):
+    logger.info(f"Started processing episode {ep_i}")
+
+    obj_ids = traj["obj_id"]
+    obj_ids = dict(obj_ids.item())
+
+    mujoco_xml = traj["mujoco_xml"]
+    all_pos = traj["pos"]
+    all_quats = traj["quat"]
+    input_seq_len = sim_cfg["input_seq_length"]
+    seq_len = all_pos.shape[0]
+    ep_len = seq_len - input_seq_len - 1
+    sim = MjSimLearned.from_xml_string(str(mujoco_xml))
+    sim.forward()
+    tracker = PhysicsStateTracker(
+        sim=sim, security_margin=sim_cfg["collision_radius"]
+    )
+    for t in range(seq_len - input_seq_len - 1):
+        # for t in tqdm.tqdm(range(seq_len - input_seq_len - 1)):
+        # Include targets
+        positions = all_pos[t : t + input_seq_len + 1, ...]
+        quaternions = all_quats[t : t + input_seq_len + 1, ...]
+        sim.set_state(
+            positions=positions[-2, ...],
+            quaternions=quaternions[-2, ...],
+            obj_ids=obj_ids,
+        )
+        collisions = tracker.detect_collisions(bidirectional=True)
+        scn_info = get_scene_info(
+            sim=sim,
+            body_meshes=tracker.body_meshes,
+            properties=tracker.properties,
+            obj_positions=positions,
+            obj_quaternions=quaternions,
+            obj_ids=obj_ids,
+            collisions=collisions,
+            contains_targets=True,
+        )
+        save_graph(scn_info, ep_len * ep_i + t, output_path)
+        with lock:
+            counter.value += 1
+
+
+def process_wrapper(args):
+    """Unpack arguments and call process_episode."""
+    # traj, sim_cfg, ep_i, lock, pbar = args
+    process_episode(*args)
+
+
 if __name__ == "__main__":
     try:
         with open(os.path.join(os.getcwd(), args.config_file)) as f:
@@ -81,26 +139,33 @@ if __name__ == "__main__":
         f"Parsing {data_path}. Preprocessed graphs will be stored in {output_path}"
     )
     try:
-        sim_cfg = config.pop("simulator")
-
-        dataset = MujocoDataset(
-            path=data_path,
-            mode="sample",
-            input_sequence_length=sim_cfg["input_seq_length"],
-            # transform=T.Compose([ToTensor(device)]),
-            collision_radius=sim_cfg["collision_radius"],
-        )
-        data_loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=batch_size,
-            shuffle=False,
-            num_workers=num_workers,
-            pin_memory=False,
-            collate_fn=collate_fn,
-        )
-        for i, sample in enumerate(
-            tqdm.tqdm(data_loader, desc="Preprocessing data")
-        ):
-            save_graph(sample, i, output_path)
+        data = list(np.load(data_path, allow_pickle=True).values())[0]
     except FileNotFoundError as e:
         print(e)
+    sim_cfg = config.pop("simulator")
+    data_len = len(data) * (
+        data[0]["pos"].shape[0] - sim_cfg["input_seq_length"] - 1
+    )
+    pbar = tqdm.tqdm(total=data_len, desc="Processing")
+
+    with Manager() as manager:
+        counter = manager.Value("i", 1)
+        lock = manager.Lock()
+        with concurrent.futures.ProcessPoolExecutor(
+            max_workers=num_workers
+        ) as executor:
+            tasks = [
+                (traj, sim_cfg, i, lock, counter)
+                for i, traj in enumerate(data)
+            ]
+
+            # Submit tasks to the executor without storing futures since no
+            # return is needed
+            for task in tasks:
+                executor.submit(process_wrapper, task)
+
+            while True:
+                pbar.n = counter.value
+                pbar.refresh()
+                if counter.value >= data_len:
+                    break
